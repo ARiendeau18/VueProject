023e6322ab702c96c6587765cc75a68cab28b0cf
ariendea <riendeauandrew@gmail.com>
2022-04-14 08:29:42 -0400

    commit1

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
+//Reference: Coding with Justin on youtube for the google maps functionality through vue.js
 import { createApp } from 'vue'
 import App from './App.vue'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).mount('#app')
