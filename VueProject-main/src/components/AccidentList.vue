df62a5daa9309da69e94b086b8b7c9d380931cc9
spoolmaster <sbrightdsi@gmail.com>
2022-05-03 23:05:06 -0400

    3times

--- VueProject-main/src/components/AccidentList.vue
+++ VueProject-main/src/components/AccidentList.vue
@@ -1,13 +1,31 @@
 <template>
-    
+    <div>
+        <div class="markers">
+            <google-map>
+                <div v-for="{ id, latitude, longitude, traffic} in accidents" :key="id">
+                    const myLatLng = { lat: {{ latitude }}, lng: {{ longitude }} };
+                    new google.maps.Marker({
+                    position: myLatLng,
+                    map,
+                    title= {{ traffic }}
+                    });  
+                </div>
+            </google-map>   
+            
+        </div>
+    </div>
 </template>
 
 <script>
-import { useLoadAccidents, deleteAccidents } from "@/db"
+import { useLoadAccidents, deleteAccident } from "@/db"
 export default {
     setup() {
         const accidents = useLoadAccidents()
-        return{accidents, deleteAccidents}
+        return{accidents, deleteAccident}
+    },
+    for(){
+
     }
+    
 }
 </script>
